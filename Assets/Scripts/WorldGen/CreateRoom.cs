ac0537862caeb33b20bd4d8e66bc70701e6b29ef
jag114 <jag200202@gmail.com>
2025-05-05 21:28:10 +0200

    TG-28
    added drunkard's walk as path creating algorithm

--- Assets/Scripts/WorldGen/CreateRoom.cs
+++ Assets/Scripts/WorldGen/CreateRoom.cs
@@ -1,145 +1,188 @@
+using Assets.Scripts.WorldGen;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static UnityEngine.Rendering.DebugUI;
 
-public class CreateRoom : MonoBehaviour
+namespace Assets.Scripts.CreateRoom
 {
-    public TilePalette tilePallete;
-    public TileBase[] tilePalleteStone;//tests
-    public struct MapData
+    public class CreateRoom : MonoBehaviour
     {
-        public int width;
-        public int height;
-        public int[][] mapArray;
-    }
-    public TileBase tile;
-    public TileBase rootTile; //tests
-    public Tilemap map;
-
-    private void Awake()
-    {
-        Debug.Log("AWAKE");
-        tilePallete.tiles = Resources.LoadAll<TileBase>("Tile Palette/TP Grass");
-        tilePalleteStone = Resources.LoadAll<TileBase>("Tile Palette/TP Wall");
-    }
-
-    void Start()
-    {
-        MapData mapData = GenerateArray(64, 64);
-        mapData = GenerateRoom(mapData);
-        RenderMap(mapData, map, tile);  
-    }
-
-    public void RenderMap(MapData mapData, Tilemap tilemap, TileBase tile)
-    {
-        tilemap.ClearAllTiles();
-        for (int x = 0; x < mapData.width; x++)
+        public TilePalette tilePallete;
+        public TileBase[] tilePalleteStone;//tests
+        public struct MapData
         {
-            for (int y = 0; y < mapData.height; y++)
-            {
-                if (mapData.mapArray[x][y] == -1)
-                {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), rootTile);
+            public int width;
+            public int height;
+            public int[][] mapArray;
+        }
+        public TileBase tile;
+        public TileBase rootTile; //tests
+        public TileBase pathTile; //tests
+        public Tilemap map;
 
-                }
-                else if(mapData.mapArray[x][y] == -2)
-                {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-                }
-                else
-                {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), tilePallete.tiles[mapData.mapArray[x][y]]);
-                }
+        private struct Room
+        {
+            public Vector2Int rootCoords;
+            public int width;
+            public int height;
+
+            public Room(Vector2Int position, int width, int height)
+            {
+                this.rootCoords = position;
+                this.width = width;
+                this.height = height;
             }
         }
-    }
+        private static List<Room> rooms;
 
-    private MapData GenerateArray(int width, int height)
-    {
-        MapData mapData = new MapData();
-        mapData.width = width;
-        mapData.height = height;
-        mapData.mapArray = new int[width][];
-
-        for(int i = 0; i < width; i++)
+        private void Awake()
         {
-            mapData.mapArray[i] = new int[height];
+            rooms = new List<Room>();
+            tilePallete.tiles = Resources.LoadAll<TileBase>("Tile Palette/TP Grass");
+            tilePalleteStone = Resources.LoadAll<TileBase>("Tile Palette/TP Wall");
         }
 
-        for (int x = 0; x < width; x++)
+        void Start()
         {
-            for (int y = 0; y < height; y++)
-            {
-                mapData.mapArray[x][y] = Mathf.RoundToInt(Random.Range(0, tilePallete.tiles.Length));
-            }
+            MapData mapData = GenerateArray(64, 64);
+            mapData = GenerateFloor(mapData);
+            mapData = GenerateCorridors(mapData);//test
+            RenderMap(mapData, map, tile);
+
         }
-        return mapData;
-    }
 
-    private static MapData GenerateRoom(MapData mapData)
-    {
-        int numberOfRooms = Mathf.RoundToInt(Random.Range(5, 10));
-
-        mapData = CreateRoomRoot(mapData, numberOfRooms);
-
-        return mapData;
-    }
-
-    private static MapData CreateRoomRoot(MapData mapData, int count, int assignValue = -1)
-    {
-        int width = mapData.width;
-        int height = mapData.height;
-
-        int regionCols = Mathf.CeilToInt(Mathf.Sqrt(count));
-        int regionRows = Mathf.CeilToInt((float)count / regionCols);
-
-        int regionWidth = width / regionCols;
-        int regionHeight = height / regionRows;
-
-        int roomWidth = Mathf.RoundToInt(Random.Range(3, 6));
-        int roomHeight = Mathf.RoundToInt(Random.Range(3, 6));
-
-        System.Random rnd = new System.Random();
-
-        for (int row = 0; row < regionRows; row++)
+        public void RenderMap(MapData mapData, Tilemap tilemap, TileBase tile)
         {
-            for (int col = 0; col < regionCols; col++)
+            tilemap.ClearAllTiles();
+            for (int x = 0; x < mapData.width; x++)
             {
-                int minX = col * regionWidth;
-                int minY = row * regionHeight;
-                int maxX = Mathf.Min(width, minX + regionWidth);
-                int maxY = Mathf.Min(height, minY + regionHeight);
-
-                if (maxX <= minX || maxY <= minY) continue;
-
-                int x = rnd.Next(minX, maxX);
-                int y = rnd.Next(minY, maxY);
-
-                int widthLimit = roomWidth;
-
-                for (int i = x; i < width - roomWidth; i++)
+                for (int y = 0; y < mapData.height; y++)
                 {
-                    if (widthLimit <= 0) break;
-
-                    int heightLimit = roomHeight;
-
-                    for (int j = y; j < height - roomHeight; j++)
+                    if (mapData.mapArray[x][y] == -1)
                     {
-                        if (heightLimit <= 0) break;
-                       
-                        mapData.mapArray[i][j] = -2;
-                        
-                        heightLimit--;
+                        tilemap.SetTile(new Vector3Int(x, y, 0), rootTile);
+
+                    }
+                    else if (mapData.mapArray[x][y] == -2)
+                    {
+                        tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                    }
+                    else if (mapData.mapArray[x][y] == -3)
+                    {
+                        tilemap.SetTile(new Vector3Int(x, y, 0), pathTile);
+                    }
+                    else
+                    {
+                        tilemap.SetTile(new Vector3Int(x, y, 0), tilePallete.tiles[mapData.mapArray[x][y]]);
+                    }
+                }
+            }
+        }
+
+        private MapData GenerateArray(int width, int height)
+        {
+            MapData mapData = new MapData();
+            mapData.width = width;
+            mapData.height = height;
+            mapData.mapArray = new int[width][];
+
+            for (int i = 0; i < width; i++)
+            {
+                mapData.mapArray[i] = new int[height];
+            }
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    mapData.mapArray[x][y] = Mathf.RoundToInt(Random.Range(0, tilePallete.tiles.Length));
+                }
+            }
+            return mapData;
+        }
+
+        private static MapData GenerateFloor(MapData mapData)
+        {
+            int numberOfRooms = Mathf.RoundToInt(Random.Range(5, 10));
+
+            mapData = GenerateRoom(mapData, numberOfRooms);
+
+            return mapData;
+        }
+
+        private static MapData GenerateRoom(MapData mapData, int count, int assignValue = -1)
+        {
+            int width = mapData.width;
+            int height = mapData.height;
+
+            int regionCols = Mathf.CeilToInt(Mathf.Sqrt(count));
+            int regionRows = Mathf.CeilToInt((float)count / regionCols);
+
+            int regionWidth = width / regionCols;
+            int regionHeight = height / regionRows;
+
+            System.Random rnd = new System.Random();
+
+            for (int row = 0; row < regionRows; row++)
+            {
+                for (int col = 0; col < regionCols; col++)
+                {
+                    int minX = col * regionWidth;
+                    int minY = row * regionHeight;
+                    int maxX = Mathf.Min(width, minX + regionWidth);
+                    int maxY = Mathf.Min(height, minY + regionHeight);
+
+                    if (maxX <= minX || maxY <= minY) continue;
+
+                    int x = rnd.Next(minX, maxX);
+                    int y = rnd.Next(minY, maxY);
+
+                    int roomWidth = Mathf.RoundToInt(Random.Range(3, 6));
+                    int roomHeight = Mathf.RoundToInt(Random.Range(3, 6));
+
+                    int widthLimit = roomWidth;
+
+                    mapData.mapArray[x][y] = assignValue; //root
+                    rooms.Add(new Room(new Vector2Int(x, y), roomWidth, roomHeight));
+
+                    for (int i = x; i < width - roomWidth; i++)
+                    {
+                        if (widthLimit <= 0) break;
+
+                        int heightLimit = roomHeight;
+
+                        for (int j = y; j < height - roomHeight; j++)
+                        {
+                            if (heightLimit <= 0) break;
+
+                            mapData.mapArray[i][j] = -2; //room
+
+                            heightLimit--;
+                        }
+
+                        widthLimit--;
                     }
 
-                    widthLimit--;
                 }
-                
-                mapData.mapArray[x][y] = assignValue;
             }
+
+            return mapData;
         }
 
-        return mapData;
+        private static MapData GenerateCorridors(MapData mapData)
+        {
+            MapData tempMapData = mapData;
+
+            foreach(Room room in rooms)
+            {
+                Vector2Int start = room.rootCoords;
+                //Vector2Int goal = rooms[j].rootCoords;
+                tempMapData = PathFinding.RandomWalk(mapData, new Vector2Int(start.x, start.y), 200);
+            }
+
+            return tempMapData;
+        }
     }
 }
+
+
---
+++ Assets/Scripts/WorldGen/PathFinding.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using static Assets.Scripts.CreateRoom.CreateRoom;
+
+namespace Assets.Scripts.WorldGen
+{
+    public static class PathFinding
+    {
+        //    drunkardWalk(grid, startX, startY, targetTileCount) :
+        //x, y = startX, startY
+        //carved = 0
+
+        //while carved<targetTileCount:
+        //    if grid[x][y] is wall:
+        //        grid[x][y] = floor
+        //        carved += 1
+
+        //    direction = random(up, down, left, right)
+
+        //    if direction == up and y > 0: y -= 1
+        //    if direction == down and y<height - 1: y += 1
+        //    if direction == left and x > 0: x -= 1
+        //    if direction == right and x<width - 1: x += 1
+
+        //return grid
+
+        //TODO
+        //add visited places
+        //add direction bias
+        public static MapData RandomWalk(MapData mapData, Vector2Int start, int lengthLimit)
+        {
+            MapData tempMapData = mapData;
+            int startX = start.x;
+            int startY = start.y;
+            int walked = 0;
+
+            int width = tempMapData.mapArray.Length;
+            int height = tempMapData.mapArray[0].Length; // assumes uniform row length
+
+            while (walked < lengthLimit)
+            {
+                // Bounds check before access
+                if (startX >= 0 && startX < width &&
+                    startY >= 0 && startY < height)
+                {
+                    if (tempMapData.mapArray[startX][startY] >= 0) // is wall 
+                    {
+                        tempMapData.mapArray[startX][startY] = -3; 
+                    }
+                }
+
+                Vector2Int lastDirection = Directions.GetRandom();
+                Vector2Int dir = lastDirection;
+                if (Random.value < 0.3f)
+                {
+                    dir = Directions.GetRandom();
+                }
+                lastDirection = dir;
+
+                // preview move
+                int nextX = startX + dir.x;
+                int nextY = startY + dir.y;
+
+                // only apply move if in bounds
+                if (nextX >= 0 && nextX < width &&
+                    nextY >= 0 && nextY < height)
+                {
+                    startX = nextX;
+                    startY = nextY;
+                }
+
+                walked++;
+            }
+
+            return tempMapData;
+        }
+
+
+        public static class Directions
+        {
+            public static Vector2Int Up = new Vector2Int(0, 1);
+            public static Vector2Int Down = new Vector2Int(0, -1);
+            public static Vector2Int Left = new Vector2Int(-1, 0);
+            public static Vector2Int Right = new Vector2Int(1, 0);
+
+            public static Vector2Int[] Cardinal =
+            {
+                Up, Down, Left, Right
+            };
+
+            private static System.Random rng = new System.Random();
+
+            public static Vector2Int GetRandom()
+            {
+                int index = rng.Next(Cardinal.Length);
+                return Cardinal[index];
+            }
+        }
+    }
+}
