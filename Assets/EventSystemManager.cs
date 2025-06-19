27328bd9499049f1e75ff4ba83c315ab45f7731c
Shiro <147532922+3hira0ri@users.noreply.github.com>
2025-06-19 17:48:13 +0200

    fixes

--- Assets/EventSystemManager.cs
+++ Assets/EventSystemManager.cs
@@ -3,14 +3,34 @@ using UnityEngine.EventSystems;
 
 public class EventSystemManager : MonoBehaviour
 {
+    private void Start()
+    {
+        var eventSystems = FindObjectsOfType<EventSystem>();
+        Debug.Log($"Liczba EventSystemów: {eventSystems.Length}");
+        foreach (var es in eventSystems)
+        {
+            Debug.Log($"EventSystem: {es.gameObject.name}, Active: {es.gameObject.activeSelf}");
+        }
+    }
     void Awake()
     {
-        if (FindObjectsOfType<EventSystem>().Length > 1)
+        var eventSystems = FindObjectsOfType<EventSystem>();
+
+        foreach (var es in eventSystems)
         {
-            Destroy(gameObject);
-            return;
+            if (es.gameObject.name != "EventManager")
+            {
+                Debug.Log($"Usuwam EventSystem: {es.gameObject.name}");
+                Destroy(es.gameObject);
+            }
+            else
+            {
+                Debug.Log($"Zostawiam EventSystem: {es.gameObject.name}");
+                DontDestroyOnLoad(es.gameObject);
+            }
         }
-        DontDestroyOnLoad(gameObject);
     }
+
+
 
 }
